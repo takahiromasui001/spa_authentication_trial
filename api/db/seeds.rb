fd70abea6b86e5c72b09da90fa977bce18775722
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-15 13:32:22 +0900

    seed作成

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env == "development"
+  User.create!(name: 'user1', email: 'user1@example.com', password: '1111', password_confirmation: '1111')
+end
