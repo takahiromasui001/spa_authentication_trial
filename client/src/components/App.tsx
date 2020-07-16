559b874f12e8be8c121919281c2e1369be14c407
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-16 19:53:10 +0900

    Helloコンポーネント表示

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,34 +1,17 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Hello from './Hello/hello'
 
 const App: React.FC = () => {
-  const [loginResult, setLoginResult] = useState({ message: '' })
-  const [currentUser, setCurrentUser] = useState({ name: '' })
-  const client = axios.create({ withCredentials: true })
-
-  const login = async () => {
-    const response = await client.post('http://127.0.0.1:3000/api/v1/login', {
-      name: 'user2',
-      password: '1111',
-    })
-    setLoginResult(response.data)
-  }
-
-  const fetchCurrentUser = async () => {
-    const response = await client.get(
-      'http://127.0.0.1:3000/api/v1/current_user'
-    )
-    setCurrentUser(response.data)
-    console.log(response.data)
-  }
-
   return (
     <div className="App">
-      <div>Learn React</div>
-      <button onClick={login}>login</button>
-      <div>loginResult={loginResult.message}</div>
-      <button onClick={fetchCurrentUser}>currentUser</button>
-      <div>currentUser={currentUser.name}</div>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Hello />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
--- client/src/components/Hello/hello.tsx
+++ client/src/components/Hello/hello.tsx
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import axios from 'axios'
 
 const Hello: React.FC = () => {
   const [currentUser, setCurrentUser] = useState({ name: '', email: '' })
 
-  const client = axios.create({ withCredentials: true })
+  const client = useMemo(() => axios.create({ withCredentials: true }), [])
   useEffect(() => {
     const fetchCurrentUser = async () => {
-      const response = await client.get(
-        'http://127.0.0.1:3000/api/v1/current_user'
-      )
+      const response = await client
+        .get('http://127.0.0.1:3000/api/v1/current_user')
+        .catch((e) => e)
+      console.log(response)
       setCurrentUser(response.data)
     }
 
     fetchCurrentUser()
-  }, [])
+  }, [client])
 
   return (
     <div>
-      <div>名前: {currentUser.name}</div>
-      <div>email: {currentUser.email}</div>
+      <div>名前: {currentUser?.name}</div>
+      <div>email: {currentUser?.email}</div>
     </div>
   )
 }
