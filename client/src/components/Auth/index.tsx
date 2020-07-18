0c1cd0ad38b6f52d606de457aeb18aa24353d2db
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-18 20:12:18 +0900

    割り切ったログイン確認

--- client/src/components/Auth/index.tsx
+++ client/src/components/Auth/index.tsx
@@ -1,22 +1,27 @@
-import React, { useContext, ReactElement } from 'react'
-import { Redirect } from 'react-router-dom'
-import { AuthContext } from '../App'
+import React, { ReactElement, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
 
 type AuthProps = {
   children?: ReactElement
 }
 
 const Auth: React.FC<AuthProps> = (props) => {
-  const { authResult } = useContext(AuthContext)
-  return (
-    <>
-      {authResult.message === 'login succeed' ? (
-        props.children
-      ) : (
-        <Redirect to={'/login'} />
-      )}
-    </>
-  )
+  const history = useHistory()
+
+  useEffect(() => {
+    const currentUser = async () => {
+      await axios
+        .get('http://127.0.0.1:3000/api/v1/current_user', {
+          withCredentials: true,
+        })
+        .catch(() => history.push('/login'))
+    }
+
+    currentUser()
+  }, [])
+
+  return <>{props.children}</>
 }
 
 export default Auth
